144ad3d7b7ddd615890eb3cc8fc37874c1827afa
Kale Franz <kale@franz.io>
2021-04-04 04:10:00 +0000

    update

--- ts07/app.py
+++ ts07/app.py
@@ -14,6 +14,7 @@ ufo_ceiling_1 = '10.0.1.111'
 ufo_under_bar = '10.0.1.112'
 ufo_under_cabinets = '10.0.1.113'
 ufo_back_bar_1 = '10.0.1.114'
+ufo_ceiling_2 = '10.0.1.200'
 
 # hue notes:
 #  hue has max 65536
@@ -40,11 +41,14 @@ def set_red():
 
     tasks = (
         (lambda: Ufo(ufo_ceiling_1).rgbw(255, 0, 0, 0).on(), ()),
+        (lambda: Ufo(ufo_ceiling_2).rgbw(255, 0, 0, 0).on(), ()),
         (lambda: Ufo(ufo_under_bar).rgbw(255, 0, 0, 0).on(), ()),
         (lambda: Ufo(ufo_under_cabinets).rgbw(255, 0, 0, 0).on(), ()),
         (lambda: Ufo(ufo_back_bar_1).rgbw(255, 0, 0, 0).on(), ()),
         (set_hue_red, (0,)),
         (set_hue_red, (1,)),
+        (set_hue_red, (2,)),
+        (set_hue_red, (3,)),
     )
     execute_tasks(tasks)
     redirect("/")
@@ -62,11 +66,39 @@ def set_green():
 
     tasks = (
         (lambda: Ufo(ufo_ceiling_1).rgbw(0, 255, 0, 0).on(), ()),
+        (lambda: Ufo(ufo_ceiling_2).rgbw(0, 255, 0, 0).on(), ()),
         (lambda: Ufo(ufo_under_bar).rgbw(0, 255, 0, 0).on(), ()),
         (lambda: Ufo(ufo_under_cabinets).rgbw(0, 255, 0, 0).on(), ()),
         (lambda: Ufo(ufo_back_bar_1).rgbw(0, 255, 0, 0).on(), ()),
         (set_hue_green, (0,)),
         (set_hue_green, (1,)),
+        (set_hue_green, (2,)),
+        (set_hue_green, (3,)),
+    )
+    execute_tasks(tasks)
+    redirect("/")
+
+
+@route('/set_green_purple')
+def set_green_purple():
+    b = Bridge(hue_ip)
+    def set_hue_green(light_id):
+        light = b.lights[light_id]
+        light.on = True
+        light.hue = 25699
+        light.saturation = 254
+        light.brightness = 253
+
+    tasks = (
+        (lambda: Ufo(ufo_ceiling_1).rgbw(0, 255, 0, 0).on(), ()),
+        (lambda: Ufo(ufo_ceiling_2).rgbw(0, 255, 0, 0).on(), ()),
+        (lambda: Ufo(ufo_under_bar).rgbw(128, 0, 128, 0).on(), ()),
+        (lambda: Ufo(ufo_under_cabinets).rgbw(0, 255, 0, 0).on(), ()),
+        (lambda: Ufo(ufo_back_bar_1).rgbw(255, 0, 255, 0).on(), ()),
+        (set_hue_green, (0,)),
+        (set_hue_green, (1,)),
+        (set_hue_green, (2,)),
+        (set_hue_green, (3,)),
     )
     execute_tasks(tasks)
     redirect("/")
@@ -84,11 +116,39 @@ def set_blue():
 
     tasks = (
         (lambda: Ufo(ufo_ceiling_1).rgbw(0, 0, 80, 0).on(), ()),
+        (lambda: Ufo(ufo_ceiling_2).rgbw(0, 0, 80, 0).on(), ()),
         (lambda: Ufo(ufo_under_bar).rgbw(0, 0, 128, 0).on(), ()),
         (lambda: Ufo(ufo_under_cabinets).rgbw(0, 0, 255, 0).on(), ()),
         (lambda: Ufo(ufo_back_bar_1).rgbw(0, 0, 255, 0).on(), ()),
         (set_hue_blue, (0,)),
         (set_hue_blue, (1,)),
+        (set_hue_blue, (2,)),
+        (set_hue_blue, (3,)),
+    )
+    execute_tasks(tasks)
+    redirect("/")
+
+
+@route('/set_purple')
+def set_purple():
+    b = Bridge(hue_ip)
+    def set_hue_blue(light_id):
+        light = b.lights[light_id]
+        light.on = True
+        light.hue = 52957
+        light.saturation = 253
+        light.brightness = 252
+
+    tasks = (
+        (lambda: Ufo(ufo_ceiling_1).rgbw(80, 0, 80, 0).on(), ()),
+        (lambda: Ufo(ufo_ceiling_2).rgbw(80, 0, 80, 0).on(), ()),
+        (lambda: Ufo(ufo_under_bar).rgbw(128, 0, 128, 0).on(), ()),
+        (lambda: Ufo(ufo_under_cabinets).rgbw(255, 0, 255, 0).on(), ()),
+        (lambda: Ufo(ufo_back_bar_1).rgbw(255, 0, 255, 0).on(), ()),
+        (set_hue_blue, (0,)),
+        (set_hue_blue, (1,)),
+        (set_hue_blue, (2,)),
+        (set_hue_blue, (3,)),
     )
     execute_tasks(tasks)
     redirect("/")
@@ -106,11 +166,14 @@ def set_light_blue():
 
     tasks = (
         (lambda: Ufo(ufo_ceiling_1).rgbw(155, 155, 255, 0).on(), ()),
+        (lambda: Ufo(ufo_ceiling_2).rgbw(155, 155, 255, 0).on(), ()),
         (lambda: Ufo(ufo_under_bar).rgbw(129, 129, 192, 0).on(), ()),
         (lambda: Ufo(ufo_under_cabinets).rgbw(155, 155, 255, 0).on(), ()),
         (lambda: Ufo(ufo_back_bar_1).rgbw(155, 155, 255, 0).on(), ()),
         (set_hue_blue, (0,)),
         (set_hue_blue, (1,)),
+        (set_hue_blue, (2,)),
+        (set_hue_blue, (3,)),
     )
     execute_tasks(tasks)
     redirect("/")
@@ -128,11 +191,39 @@ def set_white():
 
     tasks = (
         (lambda: Ufo(ufo_ceiling_1).rgbw(0, 0, 0, 192).on(), ()),
+        (lambda: Ufo(ufo_ceiling_2).rgbw(0, 0, 0, 192).on(), ()),
         (lambda: Ufo(ufo_under_bar).rgbw(0, 0, 0, 192).on(), ()),
         (lambda: Ufo(ufo_under_cabinets).rgbw(255, 255, 255, 0).on(), ()),
         (lambda: Ufo(ufo_back_bar_1).rgbw(255, 255, 255, 0).on(), ()),
         (set_hue_blue, (0,)),
         (set_hue_blue, (1,)),
+        (set_hue_blue, (2,)),
+        (set_hue_blue, (3,)),
+    )
+    execute_tasks(tasks)
+    redirect("/")
+
+
+@route('/set_bright_white')
+def set_bright_white():
+    b = Bridge(hue_ip)
+    def set_hue_blue(light_id):
+        light = b.lights[light_id]
+        light.on = True
+        light.hue = 38373
+        light.saturation = 254
+        light.brightness = 254
+
+    tasks = (
+        (lambda: Ufo(ufo_ceiling_1).rgbw(255, 255, 255, 255).on(), ()),
+        (lambda: Ufo(ufo_ceiling_2).rgbw(255, 255, 255, 255).on(), ()),
+        (lambda: Ufo(ufo_under_bar).rgbw(255, 255, 255, 255).on(), ()),
+        (lambda: Ufo(ufo_under_cabinets).rgbw(255, 255, 255, 0).on(), ()),
+        (lambda: Ufo(ufo_back_bar_1).rgbw(255, 255, 255, 0).on(), ()),
+        (set_hue_blue, (0,)),
+        (set_hue_blue, (1,)),
+        (set_hue_blue, (2,)),
+        (set_hue_blue, (3,)),
     )
     execute_tasks(tasks)
     redirect("/")
@@ -147,11 +238,14 @@ def set_off():
 
     tasks = (
         (lambda: Ufo(ufo_ceiling_1).off(), ()),
+        (lambda: Ufo(ufo_ceiling_2).off(), ()),
         (lambda: Ufo(ufo_under_bar).off(), ()),
         (lambda: Ufo(ufo_under_cabinets).off(), ()),
         (lambda: Ufo(ufo_back_bar_1).off(), ()),
         (set_hue_off, (0,)),
         (set_hue_off, (1,)),
+        (set_hue_off, (2,)),
+        (set_hue_off, (3,)),
     )
     execute_tasks(tasks)
     redirect("/")
@@ -174,6 +268,8 @@ def get_status():
     result = Ufo.all_status()
     result += get_hue_status(0)
     result += get_hue_status(1)
+    result += get_hue_status(2)
+    result += get_hue_status(3)
     response.content_type = 'text/plain'
     return result
 
@@ -195,11 +291,14 @@ index_html = """<!DOCTYPE html>
 
 <div class="container-fluid">
   <a href="/set_off" class="btn btn-outline-primary btn-lg btn-block">Off</a>
-  <a href="/set_white" class="btn btn-primary btn-lg btn-block" style="background-color: #fff; color: #000;">White</a>
-  <a href="/set_blue" class="btn btn-primary btn-lg btn-block" style="background-color: #00f;">Blue</a>
-  <a href="/set_green" class="btn btn-primary btn-lg btn-block btn-success">Green</a>
-  <a href="/set_light_blue" class="btn btn-primary btn-lg btn-block btn-info">Light Blue</a>
-  <a href="/set_red" class="btn btn-primary btn-lg btn-block btn-danger">Red</a>
+  <a href="/set_white" class="btn btn-primary btn-lg btn-block" style="background-color: #fff; color: #000; font-weight: bold;">White</a>
+  <a href="/set_blue" class="btn btn-primary btn-lg btn-block" style="background-color: #00f; font-weight: bold;">Blue</a>
+  <a href="/set_purple" class="btn btn-primary btn-lg btn-block" style="background-color: #f0f; font-weight: bold;">Purple</a>
+  <a href="/set_green" class="btn btn-primary btn-lg btn-block btn-success" style="font-weight: bold;">Green</a>
+  <a href="/set_light_blue" class="btn btn-primary btn-lg btn-block btn-info" style="font-weight: bold;">Light Blue</a>
+  <a href="/set_red" class="btn btn-primary btn-lg btn-block btn-danger" style="font-weight: bold;">Red</a>
+  <a href="/set_green_purple" class="btn btn-primary btn-lg btn-block btn-success" style="color: #f0f; font-weight: bold;">Green + Purple</a>
+  <a href="/set_bright_white" class="btn btn-primary btn-lg btn-block" style="background-color: #fff; color: #000; font-weight: bold;">Bright White</a>
 </div>
 
   </body>
@@ -208,4 +307,4 @@ index_html = """<!DOCTYPE html>
 
 
 if __name__ == "__main__":
-    run(host='0.0.0.0', port=3607)
+    run(host='0.0.0.0', port=3607, debug=True)
